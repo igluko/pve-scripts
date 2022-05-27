711deb429c9b0517baca5840fe8dc57bae1d5f8b
Evgeny Savelov <igluko@gmail.com>
2022-05-27 21:33:50 +0300

    add sync configs and show snapshot in notes

---
+++ notes.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+SCRIPT=`realpath $0`
+
+if [[ $1 == "--add2cron" ]]; then
+    TASK="* * * * * $SCRIPT"
+    if crontab -l 2>/dev/null | grep -q "$TASK"
+    then 	
+        echo "task already has been added to crontab"
+    else
+        (crontab -l 2>/dev/null; echo "$TASK") | crontab -
+    fi
+fi
+
+function getMemFree(){
+running_vm=`/usr/sbin/qm list | grep running`
+running_mem=0
+if [ -n "$running_vm" ]; then
+  running_mem=`echo "$running_vm" | awk '{print $4}' | paste -sd+ | awk '{printf "scale=0;(%s)/1024\n", ($1) }' | bc`
+fi
+
+total_mem=`cat /proc/meminfo | awk '$1=="MemTotal:" {printf "%.f", $2/1024/1024 }'`
+arc_mem=`cat /proc/spl/kstat/zfs/arcstats | awk '$1=="size" {printf "%.f", $3/1024/1024/1024 }'`
+
+echo "#Total Mem: **$total_mem GB**  "
+echo "#"
+echo "#Running VM Reserved: **$running_mem GB**  "
+echo "#"
+awk '$1=="size" {printf "#ARC SIZE = **%.f GB** \n", $3/1024/1024/1024 }' /proc/spl/kstat/zfs/arcstats
+echo "#"
+}
+
+function getSyncoidSnaps(){
+echo "#**Syncoid snapshots:**"
+echo "#"
+zfs list -t snapshot -o name | grep syncoid | awk '{printf "#%s \\\n" , $0}'
+echo "#**end**"
+}
+
+getMemFree>"/etc/pve/local/config"
+getSyncoidSnaps>>"/etc/pve/local/config"
--- sync.sh
+++ sync.sh
@@ -14,9 +14,18 @@ fi
 
 DST_NODE=$1
 SSH="ssh -o BatchMode=yes -o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q root@$DST_NODE"
+SCP="scp -o BatchMode=yes -o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q"
+SYNCOID='/usr/sbin/syncoid --sshoption="ConnectTimeout=5" --skip-parent -r --sendoptions=-wR --force-delete --recvoptions="-F -o keylocation=file:///tmp/passphrase" --exclude=".*-[6-9][0-9][0-9]-disk-[0-9]+"'
 
-eval  '/usr/sbin/syncoid --sshoption="ConnectTimeout=5" --skip-parent -r --sendoptions=-wR --force-delete \
---recvoptions="-F -o keylocation=file:///tmp/passphrase" rpool/data root@$DST_NODE:rpool/data'
+# sync configs
+eval  "$SCP -r /etc/pve/local/qemu-server/[1-5]* root@$DST_NODE:/etc/pve/local/qemu-server/ 2>/dev/null"
+eval  "$SCP -r /etc/pve/local/lxc/[1-5]* root@$DST_NODE:/etc/pve/local/lxc/ 2>/dev/null"
+
+# sync rpool/data
+eval "$SYNCOID rpool/data root@$DST_NODE:rpool/data"
+
+# sync hdd
+#eval "$SYNCOID hdd root@$DST_NODE:hdd"
 
 # load keys
 if $SSH "zfs list -H -o name,keystatus,keylocation | awk '\$2==\"unavailable\" && \$3!=\"prompt\" {print \$1}' | grep -q ."

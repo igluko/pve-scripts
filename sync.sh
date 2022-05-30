9d47d4d22fac3f9210f7de528ff72f3d8c5747c4
Evgeny Savelov <igluko@gmail.com>
2022-05-30 17:53:26 +0300

    add prefix

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # check number of parameters
-if [ $# -ne 1 ]; then
-    echo "usage: sync.sh 1.2.3.4"
+if [ $# -ne 0 ]; then
+    echo "usage: sync.sh"
     exit 1
 fi
 
@@ -12,10 +12,11 @@ if [ ! -f /usr/sbin/syncoid ]; then
     exit 1
 fi
 
-DST_NODE=$1
+DST_NODE="10.25.254.246"
+PREFIX="office"
 SSH="ssh -o BatchMode=yes -o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q root@$DST_NODE"
 SCP="scp -o BatchMode=yes -o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q"
-SYNCOID='/usr/sbin/syncoid --sshoption="ConnectTimeout=5" --skip-parent -r --sendoptions=-wR --force-delete --recvoptions="-F -o keylocation=file:///tmp/passphrase" --exclude=".*-[6-9][0-9][0-9]-disk-[0-9]+"'
+SYNCOID='/usr/sbin/syncoid --sshoption="ConnectTimeout=5" --skip-parent -r --sendoptions=-wR --force-delete --recvoptions="-F -o keylocation=file:///tmp/passphrase" --exclude=".*-[6-9][0-9][0-9]-disk-[0-9]+"  --identifier=$PREFIX'
 
 # sync configs
 eval  "$SCP -r /etc/pve/local/qemu-server/[1-5]* root@$DST_NODE:/etc/pve/local/qemu-server/ 2>/dev/null"
@@ -25,7 +26,7 @@ eval  "$SCP -r /etc/pve/local/lxc/[1-5]* root@$DST_NODE:/etc/pve/local/lxc/ 2>/d
 eval "$SYNCOID rpool/data root@$DST_NODE:rpool/data"
 
 # sync hdd
-eval "$SYNCOID hdd root@$DST_NODE:hdd"
+#eval "$SYNCOID hdd root@$DST_NODE:hdd"
 
 # load keys
 if $SSH "zfs list -H -o name,keystatus,keylocation | awk '\$2==\"unavailable\" && \$3!=\"prompt\" {print \$1}' | grep -q ."

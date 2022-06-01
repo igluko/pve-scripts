3c45b09ada55f0083ea43dd3901ce8cc64c456c6
Evgeny Savelov <igluko@gmail.com>
2022-06-01 16:08:33 +0300

    improve load-key

--- sync.sh
+++ sync.sh
@@ -9,9 +9,10 @@ if [ $# -ne 1 ]; then
 fi
 
 DST_NODE=$1
-SSH="ssh -o BatchMode=yes -o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q root@$DST_NODE"
-SCP="scp -o BatchMode=yes -o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q"
-SYNCOID='/usr/sbin/syncoid --sshoption="ConnectTimeout=5" --skip-parent -r --sendoptions=-wR --force-delete --recvoptions="-F -o keylocation=file:///tmp/passphrase" --exclude=".*-[6-9][0-9][0-9]-disk-[0-9]+"'
+SSH_OPT="BatchMode=yes -o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -q"
+SSH="ssh -o $SSH_OPT root@$DST_NODE"
+SCP="scp -o $SSH_OPT"
+SYNCOID="/usr/sbin/syncoid --sshoption=\"$SSH_OPT\" --skip-parent -r --sendoptions=-wR --force-delete --exclude=\".*-[6-9][0-9][0-9]-disk-[0-9]+\""
 
 # check syncoid is available
 if [ ! -f /usr/sbin/syncoid ]; then
@@ -64,8 +65,17 @@ eval  "$SCP -r root@$DST_NODE:/etc/pve/local/qemu-server/[1-5]* /etc/pve/local/q
 eval  "$SCP -r root@$DST_NODE:/etc/pve/local/lxc/[1-5]* /etc/pve/local/lxc/ 2>/dev/null"
 
 # load keys
-if eval "zfs list -H -o name,keystatus,keylocation | awk '\$2==\"unavailable\" && \$3!=\"prompt\" {print \$1}' | grep -q ."
-then
-    echo "Warning: found unavailable keys"
-    eval "zfs list -H -o name,keystatus,keylocation | awk '\$2==\"unavailable\" && \$3!=\"prompt\" {print \$1}' | xargs -n1 zfs load-key"
-fi
+for ZFS in $(zfs list -H -o name,keystatus,keylocation | awk '$2=="unavailable" && $3!="prompt" {print $1}')
+do
+    echo "load-key for $ZFS"
+    zfs set keylocation=file:///tmp/passphrase $ZFS
+    zfs load-key $ZFS
+done
+
+# load keys
+#if eval "zfs list -H -o name,keystatus,keylocation | awk '\$2==\"unavailable\" && \$3!=\"prompt\" {print \$1}' | grep -q ."
+#then
+#    echo "Warning: found unavailable keys"
+#    eval "zfs list -H -o name,keystatus,keylocation | awk '\$2==\"unavailable\" && \$3!=\"prompt\" {print \$1}' | xargs -n1 zfs set keylocation=file:///tmp/passphrase"
+#    eval "zfs list -H -o name,keystatus,keylocation | awk '\$2==\"unavailable\" && \$3!=\"prompt\" {print \$1}' | xargs -n1 zfs load-key"
+#fi

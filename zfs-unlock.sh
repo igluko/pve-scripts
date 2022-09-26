21aaf2bfacb7e42e77cb3a222d6d294165509c64
gluko <gluko@gmail.com>
2022-09-27 00:10:34 +0300

    add zfs-unlock.sh

---
+++ zfs-unlock.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Helpful to read output when debugging
+set -x
+# Этот скрипт загружает ключи шифрованных ZFS датасетов и томов
+# На сервере должны быть настроены глобальные переменные окружения (/etc/environment): 
+# $TXT_ZFS_KEY=ZFS_KEY (Загрузить ключ из DNS записи ZFS_KEY) 
+# $URL_ZFS_KEY=https://domain.com/secret-link
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+# read envivoments
+source  /etc/environment
+# add binary folders to local path
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+load-key() {
+  local KEY=$1
+  if [ -n $KEY ]
+  then
+    echo $KEY
+    yes $KEY | zfs load-key -a -L prompt
+  fi
+}
+
+if [[ -n "$1" ]]
+then
+  KEY=$1
+  load-key $KEY
+  exit 0
+fi
+
+if [[ -n "$TXT_ZFS_KEY" ]]
+then
+  echo "Load key from DNS TXT:$TXT_ZFS_KEY"
+  KEY=$(dig +short txt $TXT_ZFS_KEY | xargs) # use xargs for trim
+  load-key $KEY
+fi
+
+if [[ -n "$URL_ZFS_KEY" ]]
+then
+  echo "Load key from URL $URL_ZFS_KEY"
+  KEY=$(curl -s $URL_ZFS_KEY)
+  load-key $KEY
+fi

05e6859f874214c0a743a6dd4f6890a025a107f7
Evgeny Savelov <igluko@gmail.com>
2022-12-16 20:36:00 +0300

    Adding support for specifying disks in parameters.

--- fio.sh
+++ fio.sh
@@ -36,39 +36,73 @@ function apt-install {
 # Install soft
 apt-install fio
 apt-install jq
-apt-install fdisk 
+apt-install fdisk
+
 
 DISKS="nvme0n1 nvme1n1"
-PART="p4"
+PART="127"
+
+if [[ $# -ne 0 ]]
+then
+    DISKS=$*
+fi
+
+# if [[ $# -eq 0 ]]
+# then
+#     DISKS="nvme0n1 nvme1n1"
+# else
+#     DISKS=$*
+# fi
+
+# PART="127"
+
+# Выводим информацию о текущих дисках
+printf "\n${ORANGE}Информация о текущий дисках${NC}\n"
+lsblk | grep nvme
 
 for DISK in $DISKS
 do
-    # Проверяем, что раздел существует на диске
-    printf "\n${ORANGE}Проверяем, что раздел /dev/${DISK}${PART} существует${NC}\n"
-
-    if ! [ -e /dev/${DISK}${PART} ]
+    # Проверяем, что диск существует
+    printf "\n${ORANGE}Проверяем, что диск  /dev/${DISK} существует${NC}\n"
+    if ! lsblk | grep -q ${DISK}
     then
-        printf "\n${RED}Раздел ${PART} не существует, создаем его${NC}\n"
+        printf "${RED}Ошибка: диск /dev/${DISK} не найден${NC}\n"
+        continue
+    fi
+
+    # Проверяем, что раздел существует на диске. Если нет - создаем раздел.
+    printf "\n${ORANGE}Проверяем, что раздел /dev/${DISK}p${PART} существует${NC}\n"
+    if ! lsblk | grep -q ${DISK}p${PART}
+    then
+        printf "\n${ORANGE}Раздел ${PART} не существует, создаем его${NC}\n"
         (
-            echo n # Add a new partition
-            echo   # Partition number
-            echo   # First sector (Accept default: 1)
-            echo   # Last sector (Accept default: varies)
-            echo w # Write changes
+            echo n        # Add a new partition
+            echo ${PART}  # Partition number
+            echo          # First sector (Accept default: 1)
+            echo          # Last sector (Accept default: varies)
+            echo w        # Write changes
         ) | fdisk /dev/${DISK}
     fi
-    echo
+
+    # Снова проверяем, что раздел существует на диске
+    printf "\n${ORANGE}Снова проверяем, что раздел /dev/${DISK}p${PART} существует${NC}\n"
+    if ! lsblk | grep -q ${DISK}p${PART}
+    then
+        printf "${RED}Ошибка: раздел /dev/${DISK}p${PART} не найден.${NC}\n"
+        continue
+    fi
+
+    # Выводим информацию о диске и материнской плате
     hostname
     cat /sys/class/block/${DISKS}/device/model
     cat /sys/class/block/${DISK}/device/serial
     cat /sys/devices/virtual/dmi/id/board_{vendor,name}
 
-
-
     function fio-run {
-        fio --filename=/dev/${DISK}${PART} --group_reporting --output-format=json --runtime=60 --size=50G --ioengine=libaio --direct=1 --stonewall $*
+        fio --filename=/dev/${DISK}p${PART} --group_reporting --output-format=json --runtime=60 --size=50G --ioengine=libaio --direct=1 --stonewall $*
     }
-    
+
+    # Выводим заголовки тестов
     printf "\n${GREEN}Заголовки:${NC}\n"
     echo -e "Seq-1m-Q8T1-Read \t KB/sec"
     echo -e "Seq-1m-Q8T1-Write \t KB/sec"
@@ -85,6 +119,7 @@ do
     echo -e "Rnd-4k-Q1T1-Read \t iops"
     echo -e "Rnd-4k-Q1T1-Write \t iops"
 
+    # Выводим результаты тестов
     printf "\n${GREEN}Результаты:${NC}\n"
     fio-run --name=Seq-1m-Q8T1-Read --rw=read --bs=1m --iodepth=8 | jq .jobs[0].read.bw
     fio-run --name=Seq-1m-Q8T1-Write --rw=write --bs=1m --iodepth=8 | jq .jobs[0].write.bw
@@ -101,6 +136,12 @@ do
     fio-run --name=Rnd-4k-Q1T1-Read --rw=read --bs=4k --iodepth=1 | jq .jobs[0].read.iops | sed 's/\..*//'
     fio-run --name=Rnd-4k-Q1T1-Write --rw=write --bs=4k --iodepth=1 | jq .jobs[0].write.iops | sed 's/\..*//'
 
-    echo
+    # # Удаляем тестовый раздел
+    # printf "\n${ORANGE}Удаляем тестовый раздел /dev/${DISK}p${PART}${NC}\n"
+    # (
+    #     echo d       # Add a new partition
+    #     echo ${PART} # Partition number
+    #     echo w       # Write changes
+    # ) | fdisk /dev/${DISK}
 done
 

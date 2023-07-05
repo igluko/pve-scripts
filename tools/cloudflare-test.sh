fe081b27536de10e80af3d93132a485fb45689e7
Evgeny Savelov <igluko@gmail.com>
2023-07-05 16:58:00 +0300

    add test cloudflare TTL script

---
+++ tools/cloudflare-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# get real path to script
+SCRIPT=$(realpath $0)
+SCRIPT_PATH=$(dirname ${SCRIPT})
+
+# awk нужен, чтобы вырезать нули в начале числа
+HOURS=$(date +%H | awk -F: '{print +$1}')
+MINUTES=$(date +%M | awk -F: '{print +$1}')
+
+# Тест A записей
+# Перенаправление ввода нужно, чтобы запустить скрипт в неинтерактивном режиме
+${SCRIPT_PATH}/cloudflare.sh test-a.g00.link 0.0.${HOURS}.${MINUTES} < /dev/null
